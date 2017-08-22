83c9c285b54c40d129fd92718641787ec5103773
KimWooSin <wsk8921213@gmail.com>
2017-08-22 10:40:03 +0900

    project initiation

---
+++ soscanner/src/main/java/com/finalproject/soscanner/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.finalproject.soscanner.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping("/")
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		return "/main/home";
+	}
+	
+}
